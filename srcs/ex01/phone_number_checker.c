d709b6e814f46be2d7ba59d94a17324317406b35
chanhpar <chanhpar@macui-MacBookPro.local>
2022-08-24 11:01:22 +0900

    add 42 header, fix some norm errors

--- srcs/ex01/phone_number_checker.c
+++ srcs/ex01/phone_number_checker.c
@@ -1,5 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phone_number_checker.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chanhpar <chanhpar@student.42seoul.kr      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/24 10:42:28 by chanhpar          #+#    #+#             */
+/*   Updated: 2022/08/24 10:48:07 by chanhpar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <regex.h>
 #include <unistd.h>
+
+static void	write_match_string(regex_t *reg, char *input, regmatch_t *pmatch)
+{
+	int			ret;
+
+	while (1)
+	{
+		ret = regexec(reg, input, 1, pmatch, 0);
+		if (ret != 0)
+			break ;
+		write(1, input + pmatch[0].rm_so, pmatch[0].rm_eo - pmatch[0].rm_so);
+		input += pmatch[0].rm_eo;
+	}
+	write(1, "\n", 1);
+}
 
 void	phone_number_checker(char *input)
 {
@@ -19,15 +46,7 @@ void	phone_number_checker(char *input)
 		write(1, "KO\n", 3);
 	else
 	{
-		while (1)
-		{
-			ret = regexec(&reg, input, 1, pmatch, 0);
-			if (ret != 0)
-				break ;
-			write(1, input + pmatch[0].rm_so, pmatch[0].rm_eo - pmatch[0].rm_so);
-			input += pmatch[0].rm_eo;
-		}
-		write(1, "\n", 1);
+		write_match_string(&reg, input, pmatch);
 	}
 	regfree(&reg);
 }
